8eaca44107488dd1962a782d1398fde6f582010d
ZetApps <sautin.dron@gmail.com>
2018-06-21 17:13:39 +0300

    Create ContactController.vb

--- WebTemplate/Controllers/ContactController.vb
+++ WebTemplate/Controllers/ContactController.vb
@@ -9,6 +9,9 @@ Namespace Controllers
             ViewData("PageName") = "Contacts"
             ViewData("SiteName") = SiteName
             ViewData("PageHeader") = "КОНТАКТЫ"
+            ViewData("contactList") = "Заголовок1/texttexttext1/email1" & Chr(10) &
+                                      "Заголовок2/texttexttext2/email2" & Chr(10) &
+                                      "Заголовок3/texttexttext3/email3" & Chr(10)
             Return View()
         End Function
     End Class
